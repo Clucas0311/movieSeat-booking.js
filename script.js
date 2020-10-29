5c4510e74d97f8c6b83b1cb740957770438b1375
Clucas0311 <clucas0311@gmail.com>
2020-10-29 19:01:50 -0400

    Seat click event

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ const total = document.querySelector('#total');
 const movieSelect = document.querySelector('#movie');
 
 const ticketPrice = +movieSelect.value;
+
+// Update total and count
+function updateSelectedCount() {
+	const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+	const selectedSeatsCount = selectedSeats.length;
+	count.innerText = selectedSeatsCount;
+	total.innerText = selectedSeatsCount * ticketPrice;
+}
+// Seat click event
+container.addEventListener('click', (e) => {
+	if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+	}
+	{
+		e.target.classList.toggle('selected');
+		updateSelectedCount();
+	}
+});
